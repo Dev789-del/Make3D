ad8de9d8d0a499463b172fd77edf7367a87842e1
Dev789-del <huynq.bi11-113@st.usth.edu.vn>
2024-10-02 22:09:08 +0700

    cook

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,9 @@
-import restart from 'vite-plugin-restart'
+// vite.config.js
+import { defineConfig } from 'vite';
 
-export default {
-    root: 'src/', // Sources files (typically where index.html is)
-    publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
-    server:
-    {
-        host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
-    },
-    build:
-    {
-        outDir: '../dist', // Output in the dist/ folder
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        return 'vendor';
-                    }
-                }
-            }
-        },
-        emptyOutDir: true, // Empty the folder first
-        sourcemap: true, // Add sourcemap
-        chunkSizeWarningLimit: 1000 // Adjust chunk size limit
-    },
-    plugins:
-    [
-        restart({ restart: [ '../static/**', ] }) // Restart server on static file change
-    ],
-}
+export default defineConfig({
+  server: {
+    host: 'usthmap.edu.vn', // Replace with your custom URL
+    port: 3000, // Optional: specify the port
+  },
+});
