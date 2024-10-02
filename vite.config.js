733eee82d1dccd4ad658102115ed20005c0f41d4
Dev789-del <huynq.bi11-113@st.usth.edu.vn>
2024-10-02 21:49:59 +0700

    dist

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import restart from 'vite-plugin-restart'
 
 export default {
     root: 'src/', // Sources files (typically where index.html is)
-    publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
     server:
     {
         host: true, // Open to local network and display URL
@@ -10,12 +9,17 @@ export default {
     },
     build:
     {
-        outDir: '../dist', // Output in the dist/ folder
+         // Output in the dist/ folder
+         manualChunks(id) {
+            if (id.includes('node_modules')) {
+                return 'vendor';
+            }
+         },
         emptyOutDir: true, // Empty the folder first
         sourcemap: true // Add sourcemap
     },
     plugins:
     [
-        restart({ restart: [ '../static/**', ] }) // Restart server on static file change
+        restart({ restart: [ '../src/**', ] }) // Restart server on src file change
     ],
 }
